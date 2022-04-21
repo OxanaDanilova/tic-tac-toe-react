2e6242879058a3dcc8ca499314f09e2a31720316
Oxana Danilova <oxanadanilova11@gmail.com>
2022-04-21 14:28:04 +0200

    feat: create Board component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Board } from "./components/Board/Board";
 
 function App() {
   return (
     <div className="App">
       <h1>Tic-Tac-Toe</h1>
+      <Board />
     </div>
   );
 }
