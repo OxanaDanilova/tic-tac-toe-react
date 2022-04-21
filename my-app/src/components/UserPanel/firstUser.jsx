2e74d351f1b74d726de87fe7f8885e5af7827772
Oxana Danilova <oxanadanilova11@gmail.com>
2022-04-21 15:24:15 +0200

    feat: create User Panel component

---
+++ my-app/src/components/UserPanel/firstUser.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class FirstUserPanel extends React.Component {
+  render() {
+    return (
+      <section className="first-user-panel">
+        <p>User 1</p>
+        <p>Score: </p>
+      </section>
+    );
+  }
+}
