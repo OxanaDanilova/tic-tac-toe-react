64b1feaa4099d96b0d2077ae8022535223fe7d39
Oxana Danilova <oxanadanilova11@gmail.com>
2022-04-27 11:58:12 +0200

    feat: show results in console after clicking on the Board

--- my-app/src/components/Board/Board.jsx
+++ my-app/src/components/Board/Board.jsx
@@ -1,17 +1,26 @@
+import React from "react";
+
 import "./Board.css";
 
-export function Board() {
-  return (
-    <section class="board">
-      <div id="1"></div>
-      <div id="2"></div>
-      <div id="3"></div>
-      <div id="4"></div>
-      <div id="5"></div>
-      <div id="6"></div>
-      <div id="7"></div>
-      <div id="8"></div>
-      <div id="9"></div>
-    </section>
-  );
+export class Board extends React.Component {
+  clickHandle(e) {
+    if (e.target.id) {
+      console.log(e.target.id);
+    }
+  }
+  render() {
+    return (
+      <section onClick={this.clickHandle} className="board">
+        <div id="1"></div>
+        <div id="2"></div>
+        <div id="3"></div>
+        <div id="4"></div>
+        <div id="5"></div>
+        <div id="6"></div>
+        <div id="7"></div>
+        <div id="8"></div>
+        <div id="9"></div>
+      </section>
+    );
+  }
 }
