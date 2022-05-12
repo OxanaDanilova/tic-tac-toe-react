aae2b040e4df0f8e2708d4faeba98f1cb45f2a7f
Oxana Danilova <oxanadanilova11@gmail.com>
2022-05-12 16:02:28 +0200

    feat: check if anyone has won

--- my-app/src/components/Board/Board.js
+++ my-app/src/components/Board/Board.js
@@ -7,21 +7,80 @@ export class Board extends React.Component {
     super();
     this.state = {
       gameStatus: "x",
-      boardArr: ["", "", "", "", "x", "o", "", "", "x"],
+      boardArr: ["", "", "", "", "", "", "", "", ""],
+      winner: "",
     };
   }
-  clickHandle(e, { gameStatus, boardArr }) {
-    if (e.target.id) {
-      let index = e.target.id;
-      if (boardArr[index] === "") {
-        console.log(e.target);
-        e.target.innerHTML = gameStatus;
-        boardArr[index] = gameStatus;
-        gameStatus = gameStatus === "x" ? "o" : "x";
-        this.setState({
-          gameStatus: gameStatus,
-          boardArr: boardArr,
-        });
+  isFinished(boardArr) {
+    let winner;
+    if (
+      boardArr[0] === boardArr[1] &&
+      boardArr[1] === boardArr[2] &&
+      boardArr[0] !== ""
+    )
+      winner = boardArr[0];
+    else if (
+      boardArr[3] === boardArr[4] &&
+      boardArr[4] === boardArr[5] &&
+      boardArr[3] !== ""
+    )
+      winner = boardArr[3];
+    else if (
+      boardArr[6] === boardArr[7] &&
+      boardArr[7] === boardArr[8] &&
+      boardArr[6] !== ""
+    )
+      winner = boardArr[6];
+    else if (
+      boardArr[0] === boardArr[3] &&
+      boardArr[3] === boardArr[6] &&
+      boardArr[0] !== ""
+    )
+      winner = boardArr[0];
+    else if (
+      boardArr[1] === boardArr[4] &&
+      boardArr[4] === boardArr[7] &&
+      boardArr[1] !== ""
+    )
+      winner = boardArr[1];
+    else if (
+      boardArr[2] === boardArr[5] &&
+      boardArr[5] === boardArr[8] &&
+      boardArr[2] !== ""
+    )
+      winner = boardArr[2];
+    else if (
+      boardArr[0] === boardArr[4] &&
+      boardArr[4] === boardArr[8] &&
+      boardArr[0] !== ""
+    )
+      winner = boardArr[0];
+    else if (
+      boardArr[2] === boardArr[4] &&
+      boardArr[4] === boardArr[6] &&
+      boardArr[2] !== ""
+    )
+      winner = boardArr[2];
+    if (winner) {
+      console.log(`Game is finished. The winner is ${winner}`);
+      this.setState({ winner: winner });
+    }
+  }
+  clickHandle(e, { gameStatus, boardArr, winner }) {
+    if (boardArr.includes("") && !winner) {
+      if (e.target.id) {
+        let index = e.target.id;
+        if (boardArr[index] === "") {
+          console.log(e.target);
+          e.target.innerHTML = gameStatus;
+          boardArr[index] = gameStatus;
+          gameStatus = gameStatus === "x" ? "o" : "x";
+          this.setState({
+            gameStatus: gameStatus,
+            boardArr: boardArr,
+          });
+          this.isFinished(boardArr);
+        }
       }
     }
   }
