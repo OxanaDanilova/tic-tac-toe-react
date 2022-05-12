a389158e5512df732e70b17ba349502397240ff1
Oxana Danilova <oxanadanilova11@gmail.com>
2022-05-12 14:47:40 +0200

    feat: render board according to gameArray

--- my-app/src/components/Board/Board.js
+++ my-app/src/components/Board/Board.js
@@ -3,23 +3,32 @@ import React from "react";
 import "./Board.css";
 
 export class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      gameStatus: "x",
+      boardArr: ["x", "o", "", "", "x", "o", "", "", "x"],
+    };
+  }
   clickHandle(e) {
     if (e.target.id) {
-      console.log(e.target.id);
+      console.log(e.target);
+      //e.target.innerHTML = this.state.gameStatus;
+
+      //this.gameStatus = this.gameStatus === "x" ? "o" : "x";
     }
   }
   render() {
     return (
       <section onClick={this.clickHandle} className="board">
-        <div id="1"></div>
-        <div id="2"></div>
-        <div id="3"></div>
-        <div id="4"></div>
-        <div id="5"></div>
-        <div id="6"></div>
-        <div id="7"></div>
-        <div id="8"></div>
-        <div id="9"></div>
+        {this.state.boardArr.map(
+          (el, i) =>
+            (el = (
+              <div key={i} id={i}>
+                {this.state.boardArr[i]}
+              </div>
+            ))
+        )}
       </section>
     );
   }
