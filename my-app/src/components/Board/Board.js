d0eb660874b5b698d24bd1cdbbb8ce5c758f7691
Oxana Danilova <oxanadanilova11@gmail.com>
2022-06-10 11:24:08 +0200

    feat: change color for win-boxes on the board

--- my-app/src/components/Board/Board.js
+++ my-app/src/components/Board/Board.js
@@ -8,66 +8,74 @@ export class Board extends React.Component {
     this.state = {
       gameStatus: "x",
       boardArr: ["", "", "", "", "", "", "", "", ""],
-      winner: "",
+      winner: [],
     };
   }
   isFinished(boardArr) {
-    let winner;
+    const winner = [];
     if (
       boardArr[0] === boardArr[1] &&
       boardArr[1] === boardArr[2] &&
       boardArr[0] !== ""
-    )
-      winner = boardArr[0];
-    else if (
+    ) {
+      winner.push(0, 1, 2);
+    } else if (
       boardArr[3] === boardArr[4] &&
       boardArr[4] === boardArr[5] &&
       boardArr[3] !== ""
     )
-      winner = boardArr[3];
+      winner.push(3, 4, 5);
     else if (
       boardArr[6] === boardArr[7] &&
       boardArr[7] === boardArr[8] &&
       boardArr[6] !== ""
     )
-      winner = boardArr[6];
+      winner.push(6, 7, 8);
     else if (
       boardArr[0] === boardArr[3] &&
       boardArr[3] === boardArr[6] &&
       boardArr[0] !== ""
     )
-      winner = boardArr[0];
+      winner.push(0, 3, 6);
     else if (
       boardArr[1] === boardArr[4] &&
       boardArr[4] === boardArr[7] &&
       boardArr[1] !== ""
     )
-      winner = boardArr[1];
+      winner.push(1, 4, 7);
     else if (
       boardArr[2] === boardArr[5] &&
       boardArr[5] === boardArr[8] &&
       boardArr[2] !== ""
     )
-      winner = boardArr[2];
+      winner.push(2, 5, 8);
     else if (
       boardArr[0] === boardArr[4] &&
       boardArr[4] === boardArr[8] &&
       boardArr[0] !== ""
     )
-      winner = boardArr[0];
+      winner.push(0, 4, 8);
     else if (
       boardArr[2] === boardArr[4] &&
       boardArr[4] === boardArr[6] &&
       boardArr[2] !== ""
     )
-      winner = boardArr[2];
-    if (winner) {
-      alert(`Game is finished. The winner is ${winner}`);
+      winner.push(2, 4, 6);
+    if (winner.length) {
+      console.log(`Game is finished. The winner is ${boardArr[winner[0]]}`);
       this.setState({ winner: winner });
+      winner.forEach((elem) => {
+        if (document.querySelectorAll(".board>div")[`${elem}`]) {
+          const box = document.querySelectorAll(".board>div")[`${elem}`];
+          box.classList.add("win-color");
+        }
+      });
+    } else if (!boardArr.includes("")) {
+      alert("Game is finished!");
     }
   }
   clickHandle(e, { gameStatus, boardArr, winner }) {
-    if (boardArr.includes("") && !winner) {
+    if (boardArr.includes("") && !winner.length) {
       if (e.target.id) {
         let index = e.target.id;
         if (boardArr[index] === "") {
